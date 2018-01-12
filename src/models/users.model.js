679fb26750f774bd2dcd027ae4a938b49d645e93
ciyuxu <ciyuxu@gmail.com>
2018-01-12 16:43:10 +0100

    Added Authentication

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,16 +1,15 @@
 // users-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const users = new Schema({
-    name: { type: String, required: true },
-    gender: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    addressIds: [{ type: Schema.Types.ObjectId, ref: 'addresses' }],
+  const users = new mongooseClient.Schema({
+  
+    email: {type: String, unique: true},
+    password: { type: String },
+  
+  
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
